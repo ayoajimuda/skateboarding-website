2fe6da12a729cbcac40589eea1e141a1a22a33a5
unknown <aydexter05@gmail.com>
2026-07-24 21:56:26 +0100

    Header & Footer done

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import Link from 'next/link';
+import { Container, Row, Col, Stack, Image } from 'react-bootstrap';
 
 const Footer = () => {
-    return (<footer>
-        {/* Footer Section of the website*/}
-    </footer>);
+    return (
+        <footer>
+            <Container fluid>
+                <Row className="bg-primary text-white p-4">
+                    <Col className="mx-5">
+                        <Stack>
+                            <Image
+                                src="/images/logo.png"
+                                alt="Slam.com logo"
+                                rounded
+                                width={150}
+                                height={150}
+                            />
+                            <h2>Slam.com</h2>
+                            <p>Drop hard</p>
+                        </Stack>
+                    </Col>
+                    <Col>
+                        <nav className="flex-column fs-5">
+                            <h4>Useful Links</h4>
+                            <ul>
+                                <li><Link href="/" className="text-white">Home</Link></li>
+                                <li><Link href="/about" className="text-white">About</Link></li>
+                                <li><Link href="/products" className="text-white">Products</Link></li>
+                                <li><Link href="/help" className="text-white">Help Us</Link></li>
+                            </ul>
+                        </nav>
+                    </Col>
+                    <Col>
+                        <h3>Contact the Developer!</h3>
+                        <p>aydexter05@gmail.com</p>
+                        <p>Phone: +44 7939-401-982</p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
 };
 
 export default Footer;
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import styles from '../styles/Header.module.css';
 
 const Header = () => {
     return (
-        <header className="navbar">
-            <div className="logo">
-                <Image src="/images/logo.png" alt="Slam Logo" className="slam" width={120} height={40} />
+        <header className={styles.navbar}>
+            <div className={styles.logo}>
+                <Image src="/images/logo.png" alt="Slam Logo" className={styles.slam} width={120} height={40} />
             </div>
-            <nav className="nav">
+            <nav className={styles.nav}>
                 <ul>
                     <li><Link href="/">Home</Link></li>
                     <li><Link href="/tricks">Tricks Library</Link></li>
                     <li><Link href="/spots">Skate Spots</Link></li>
-                    <li><Link href="/guides">Guides/Blogs</Link></li>
+                    <li><Link href="/guides">Guides & Blogs</Link></li>
                     <li><Link href="/customize">Customize</Link></li>
                     <li><Link href="/connect">Connect</Link></li>
                 </ul>
             </nav>
-            <button className="profile-btn">Profile</button>
+            <button className={styles.profileButton}>
+                <svg viewBox="0 0 24 24">
+                    <path d="M12 12c2.7 0 5-2.3 5-5s-2.3-5-5-5-5 2.3-5 5 2.3 5 5 5zm0 2c-3.3 0-10 1.7-10 5v3h20v-3c0-3.3-6.7-5-10-5z"/>
+                </svg>
+            </button>
         </header>
     );
 };
